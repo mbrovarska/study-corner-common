aabf0ef9b67c74b3b511abdcbeb6aa7364580a61
mbrovarska <marybrovarska@gmail.com>
2025-11-14 12:11:04 -0500

    logger import bug fixed

--- internal/logger/module.go
+++ internal/logger/module.go
@@ -2,21 +2,19 @@ package logger
 
 import (
 	"os"
-	"study-corner-common/pkg/log"
+	log "study-corner-common/pkg/logger"
 
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
 
-//use Params to inject runtime information like service name
 type Params struct {
-	fx.In
-	// Injected via fx.Provide(fx.Annotate(func() string { return "my-service" }, fx.ResultTags(`name:"service-name"`)))
-	ServiceName string `name:"service-name"`
+    fx.In
+    ServiceName string `name:"service-name"`
 }
 
 type zapLogger struct {
-	l *zap.Logger
+    l *zap.Logger
 }
 
 func (z *zapLogger) Debug(msg string, fields ...log.Field) {
@@ -36,28 +34,28 @@ func (z *zapLogger) With(fields ...log.Field) log.Logger {
 }
 
 func toZapFields(fields []log.Field) []zap.Field {
-	out := make([]zap.Field, len(fields))
-	for i, f := range fields {
-		out[i] = zap.Any(f.Key, f.Value)
-	}
-	return out
+    out := make([]zap.Field, len(fields))
+    for i, f := range fields {
+        out[i] = zap.Any(f.Key, f.Value)
+    }
+    return out
 }
 
+// Fx module: provides log.Logger based on zap.
 func Module() fx.Option {
-	return fx.Provide(
-		func (p Params) (log.Logger, error) {
-			cfg := zap.NewProductionConfig()
-			cfg.OutputPaths = []string{"stdout"}
-			cfg.InitialFields = map[string]interface{}{
-				"service": p.ServiceName,
-				"env": os.Getenv("APP_ENV"),
-			}
-			l, err := cfg.Build()
-			if err != nil {
-				return nil, err
-			}
-			return &zapLogger{l: l}, nil
-
-		},
-	)
+    return fx.Provide(
+        func(p Params) (log.Logger, error) {
+            cfg := zap.NewProductionConfig()
+            cfg.OutputPaths = []string{"stdout"}
+            cfg.InitialFields = map[string]interface{}{
+                "service": p.ServiceName,
+                "env":     os.Getenv("APP_ENV"),
+            }
+            l, err := cfg.Build()
+            if err != nil {
+                return nil, err
+            }
+            return &zapLogger{l: l}, nil
+        },
+    )
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,11 +1,12 @@
 package log
 
+// Field is a simple key/value wrapper that does not depend on zap
 type Field struct {
-	Key string
-	Value any
+    Key   string
+    Value any
 }
 
-// Logger is the interface microservices can depend on from core code
+// Logger is the interface that microservices can depend on from core code
 type Logger interface {
     Debug(msg string, fields ...Field)
     Info(msg string, fields ...Field)
@@ -14,7 +15,7 @@ type Logger interface {
     With(fields ...Field) Logger
 }
 
-
+// Helper constructors for fields so core code looks nice
 func String(key, value string) Field {
     return Field{Key: key, Value: value}
 }
@@ -22,3 +23,5 @@ func String(key, value string) Field {
 func Int64(key string, value int64) Field {
     return Field{Key: key, Value: value}
 }
+
+
